eba9ca2d72fda2fc3552df3d24f41781e65d9ad5
Thomas10111 <mrfisch27@gmail.com>
2016-06-08 16:47:53 -0700

    speed up plotting quadcopter state: split into a 2D and 3D figure 3D can enabled/disabled

--- simulator.py
+++ simulator.py
@@ -11,14 +11,14 @@ rotation = Rotation.Rotation()
 
 def controlInput(x, y, t):
     if t == 2:
-        inc = 10
+        inc = 1
     else:
         inc = 0
 
     inp = numpy.zeros((4))
-    inp[:] = 290
+    inp[:] = 288
+    inp[0] += inc
     inp[2] += inc
-    inp[3] += inc
     inp = inp**2;
     return inp
         
@@ -70,10 +70,13 @@ def omega2thetadot(omega, theta):
 
 
 plt.ion()
+enable3d = False
+
 # Simulation times, in seconds.
 start_time = 0  #s
 end_time = 10   #s
-dt = 0.1      
+dt = 0.1
+      
 times = numpy.arange(start_time, end_time, dt)
 
 m = 0.1 #kg
@@ -101,29 +104,36 @@ theta = numpy.zeros((3, 1))
 deviation = 100
 #thetadot = numpy.radians(2 * deviation * numpy.random.rand(3, 1) - deviation)
 thetadot = numpy.zeros((3,1))
-print ("thetadot: ", thetadot)
 
+print ("thetadot: ", thetadot)
 
 xpos, ypos, zpos = 0,0,0
 
 fig = plt.figure(figsize=(10,16))
-
-ax = fig.add_subplot(311, projection='3d')
-ax_xyplane = fig.add_subplot(312)
-ax_xzplane = fig.add_subplot(313)
+ax_xyplane = fig.add_subplot(211)
+ax_xzplane = fig.add_subplot(212)
 plt.tight_layout(pad=4.0, w_pad=2.0, h_pad=2.0)
- 
-line, = ax.plot( [xpos], [ypos], [zpos], "bo")
-line1, = ax_xyplane.plot(xpos, ypos, "go")
-line2, = ax_xzplane.plot(xpos, ypos, "go")
 
-ax_xyplane.set_ylim([-10,10])
-ax_xyplane.set_xlim([-10,10])
+if enable3d:
+    fig3d = plt.figure(figsize=(10,16))
+    ax3d = fig3d.add_subplot(111, projection='3d')
+    plt.figure(2)
+ 
+line1_1, = ax_xyplane.plot(xpos, ypos, "b") # vector in x-direction 
+line1_2, = ax_xyplane.plot(xpos, ypos, "g") # vector in y-direction 
+line2_1, = ax_xzplane.plot(xpos, ypos, "r") # force in z-direction
+line2_2, = ax_xzplane.plot(xpos, ypos, "b") # vector in z-direction
+
+ax_xyplane.set_ylim([-5,5])
+ax_xyplane.set_xlim([-8,8])
+ax_xyplane.set_title("XY - plane")
 ax_xyplane.set_xlabel("X")
 ax_xyplane.set_ylabel("Y")
 
-ax_xzplane.set_ylim([-10,10])
-ax_xzplane.set_xlim([-10,10])
+
+ax_xzplane.set_ylim([-5,5])
+ax_xzplane.set_xlim([-8,8])
+ax_xzplane.set_title("XZ - plane")
 ax_xzplane.set_xlabel("X")
 ax_xzplane.set_ylabel("Z")
 
@@ -148,61 +158,56 @@ for t in times:
     theta = theta + dt * thetadot
     xdot = xdot + dt * a
     x = x + dt * xdot
-     
-    print("a: ", a)
-    print("theta: ", theta)
- 
-    ax.cla()
+    
     xpos, ypos, zpos = x[0][0], x[1][0], x[2][0]
      
-    ax.set_ylim([-10, 10])
-    ax.set_xlim([-10, 10])
-    ax.set_zlim([ 0, 5])
-    ax.plot( [xpos], [ypos], [zpos], "bo")
-    ax.plot( [xpos, xpos + a[0]], [ypos, ypos+a[1]] , [zpos, zpos+a[2]],"g-",linewidth=2)
-    ax.set_title( "x: " + str(xpos) + "   y: " + str(ypos) + "   z: " + str(zpos) + "  t: " + str(t) )
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    ax.set_zlabel("Z")
+    print("a: ", a)
+#    print("theta: ", theta)
+ 
+    if enable3d:
+        ax3d.cla()
+        ax3d.set_ylim([-10, 10])
+        ax3d.set_xlim([-10, 10])
+        ax3d.set_zlim([ 0, 5])
+        ax3d.plot( [xpos], [ypos], [zpos], "bo")
+        ax3d.plot( [xpos, xpos + a[0]], [ypos, ypos+a[1]] , [zpos, zpos+a[2]],"g-",linewidth=2)
+        ax3d.set_title( "x: " + str(xpos) + "   y: " + str(ypos) + "   z: " + str(zpos) + "  t: " + str(t) )
+        ax3d.set_xlabel("X")
+        ax3d.set_ylabel("Y")
+        ax3d.set_zlabel("Z")
+        plt.draw()
     
     R = rotation.rotate(theta)
     xBody_current = R.dot(xBody)
     yBody_current = R.dot(yBody)
     zBody_current = R.dot(zBody)
-    
-    ax_xyplane.cla()
-    ax_xyplane.set_xlabel("X")
-    ax_xyplane.set_ylabel("Y")
-    ax_xyplane.set_ylim([-5,5])
-    ax_xyplane.set_xlim([-5,5])
 
-    line1.set_xdata(xpos)
-    line1.set_ydata(ypos)
-    ax_xyplane.plot([xpos, xpos + xBody_current[0]],[ypos, ypos+xBody_current[1]])
-    ax_xyplane.plot([xpos, xpos + yBody_current[0]],[ypos, ypos+yBody_current[1]])
-   
-    # xz plane
-    ax_xzplane.cla()
-    ax_xzplane.set_xlabel("X")
-    ax_xzplane.set_ylabel("Z")
-    ax_xzplane.set_ylim([0, 5])
-    ax_xzplane.set_xlim([-2, 2])
-    ax_xzplane.plot([xpos, xpos + a[0]],[zpos, zpos+a[2]], "r")
-    ax_xzplane.plot([xpos, xpos + zBody_current[0]],[zpos, zpos+zBody_current[2]],"b")
-    #ax_xzplane.plot([xpos, xpos + xBody_current[0]],[zpos, zpos+xBody_current[2]], "g")
-    line2.set_xdata(xpos)
-    line2.set_ydata(zpos)
+    ax_xyplane.set_title("XY - plane: " +str(xpos) + ", " + str(ypos) +"    t: " + str(t))
+    line1_1.set_xdata([xpos, xpos + xBody_current[0]])
+    line1_1.set_ydata([ypos, ypos + xBody_current[1]])
+    line1_2.set_xdata([xpos, xpos + yBody_current[0]])
+    line1_2.set_ydata([ypos, ypos + yBody_current[1]])
+
+    line2_1.set_xdata([xpos, 4*(xpos + a[0])])
+    line2_1.set_ydata([zpos, 4*(zpos + a[2])])
+    line2_2.set_xdata([xpos, xpos + zBody_current[0]])
+    line2_2.set_ydata([zpos, zpos + zBody_current[2]])
 #    ax_xzplane.plot(xpos, zpos, "go")
- 
-    plt.draw()
+    #
+    fig.canvas.draw()
+#    fig.canvas.flush_events() #needed?
     
 # --------------------------------------------------------------------------------------------    
     
     
+ 
+ #   ax_xzplane.plot([xpos, xpos + a[0]],[zpos, zpos+a[2]], "r")
+ #   ax_xzplane.plot([xpos, xpos + zBody_current[0]],[zpos, zpos+zBody_current[2]],"b")
+    #ax_xzplane.plot([xpos, xpos + xBody_current[0]],[zpos, zpos+xBody_current[2]], "g")    
     
     
-    
-    
+#    ax_xyplane.plot([xpos, xpos + xBody_current[0]],[ypos, ypos+xBody_current[1]])
+#    ax_xyplane.plot([xpos, xpos + yBody_current[0]],[ypos, ypos+yBody_current[1]])  
     
 # def rotate(theta):
 #     R_x = numpy.array([[               1,               0,                0],
